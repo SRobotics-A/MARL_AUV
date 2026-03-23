eec1eaf8db3e3b273f1c51cb3d8a9268351f6fab
stephen0727 <tjxu4914@tju.edu.cn>
2026-03-23 16:09:10 +0800

    chore: 补充 move_flyfollow __init__ 和 ppo cfg 文件

---
+++ exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/directMARL/move_flyfollow/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Move MARL environment.
+"""
+
+import gymnasium as gym
+
+from . import agents
+
+
+gym.register(
+    id="Isaac-move-flyfollow-marl-v0",
+    entry_point=f"{__name__}.marl_move_env:MARLMoveEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.marl_move_env_cfg:MARLMoveEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "skrl_mappo_cfg_entry_point": f"{agents.__name__}:skrl_mappo_cfg.yaml",
+    },
+)
